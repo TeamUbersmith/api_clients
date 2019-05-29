ce7334185321fd0b8606733b9fcba07acbef4335
Michael Styne <mstyne@ubersmith.com>
2019-05-29 14:42:21 -0400

    - update to v1.3

--- php/class.uber_api_client.php
+++ php/class.uber_api_client.php
@@ -2,8 +2,8 @@
 
 class uber_api_client
 {
-	const VERSION = '1.2';
-	
+	const VERSION = '1.3';
+
 	protected $options = array(
 		'debug'       => false,
 		'timeout'     => 30,
@@ -17,17 +17,17 @@ class uber_api_client
 		'orig_user'   => null,
 		'orig_ip'     => null,
 	);
-	
+
 	// value of the content type response header
 	protected $content_type;
-	
+
 	// value of the content length response header
 	protected $content_size;
-	
+
 	// value of the filename value in the content disposition response header
 	protected $content_filename;
-	
-	
+
+
 	public function __construct($url = null,$username = null,$api_token = null)
 	{
 		if (isset($url)) {
@@ -37,7 +37,7 @@ class uber_api_client
 			$this->options['userpwd'] = $username .':'. $api_token;
 		}
 	}
-	
+
 	/**
 	 * Set an option
 	 *
@@ -48,7 +48,7 @@ class uber_api_client
 	{
 		return $this->options[$option] = $value;
 	}
-	
+
 	/**
 	 * Get option(s)
 	 *
@@ -61,18 +61,18 @@ class uber_api_client
 		if (!isset($option)) {
 			return $this->options;
 		}
-		
+
 		if (isset($this->options[$option])) {
 			return $this->options[$option];
 		}
-		
+
 		if (isset($default)) {
 			return $default;
 		}
-		
+
 		return null;
 	}
-	
+
 	public function get_content_type()
 	{
 		return $this->content_type;
@@ -85,31 +85,31 @@ class uber_api_client
 	{
 		return $this->content_filename;
 	}
-	
+
 	public function call($method = 'uber.method_list',$params = array())
 	{
 		// curl library is required
 		if (!extension_loaded('curl')) {
 			return $this->raiseError('cURL support is required',1);
 		}
-		
+
 		$this->debug('API:',$this);
-		
+
 		$headers = array(
 			'Accept-Encoding: gzip',
 			'Expect:',
 		);
-		
+
 		$url = rtrim($this->options['server'],'/') .'/api/2.0/?method='. urlencode($method);
-		
+
 		if ($this->get_option('format')) {
 			$url .= '&format='. urlencode($this->get_option('format'));
 		}
-		
+
 		// if we're using json request format
 		if ($this->get_option('json_req')) {
 			$headers[] = 'Content-type: application/json';
-			
+
 			if (is_array($params)) {
 				$params = json_encode($params);
 			}
@@ -119,14 +119,22 @@ class uber_api_client
 				$params = $this->curl_postfields($params);
 			}
 		}
-		
+
 		if ($this->get_option('orig_user')) {
 			$headers[] = 'X-Ubersmith-Orig-User: '. $this->get_option('orig_user');
 		}
 		if ($this->get_option('orig_ip')) {
 			$headers[] = 'X-Ubersmith-Orig-IP: '. $this->get_option('orig_ip');
 		}
-		
+
+		if (is_array($params) && function_exists('curl_file_create')) {
+			foreach ($params as &$value) {
+				if (strpos($value, '@') === 0) {
+					$value = curl_file_create(ltrim($value, '@'));
+				}
+			}
+		}
+
 		$curl = curl_init($url);
 		curl_setopt($curl,CURLOPT_POST,1);
 		$this->debug('URL:',$url);
@@ -143,48 +151,48 @@ class uber_api_client
 		curl_setopt($curl,CURLOPT_FOLLOWLOCATION,1);
 		curl_setopt($curl,CURLOPT_MAXREDIRS,     2);
 		curl_setopt($curl,CURLOPT_HEADERFUNCTION,array($this, 'read_header'));
-		
+
 		// set auth stuff
 		$userpwd = $this->get_option('userpwd');
 		if (!empty($userpwd)) {
 			curl_setopt($curl, CURLOPT_USERPWD, $userpwd);
 		}
-		
+
 		// ssl options
 		if (substr($url,0,5) === 'https') {
 			// set CA file
 			if (file_exists(dirname(__FILE__) .'/cacert.pem')) {
 				curl_setopt($curl,CURLOPT_CAINFO,dirname(__FILE__) .'/cacert.pem');
 			}
-			
+
 			// ssl client certificate and password
 			$certificate = $this->get_option('certificate');
 			if (!empty($certificate)) {
 				curl_setopt($curl,CURLOPT_SSLCERT,$certificate);
-				
+
 				$certpass = $this->get_option('certpass');
 				if (!empty($certpass)) {
 					curl_setopt($curl,CURLOPT_SSLCERTPASSWD,$certpass);
 				}
 			}
 		}
-		
+
 		$response = curl_exec($curl);
-		
+
 		if ($response === false) {
 			$errnum = curl_errno($curl);
 			$errstr = curl_error($curl);
 			curl_close($curl);
-			
+
 			$this->debug('cURL Error:', $errstr);
 			return $this->raiseError('cURL Error: '. $errstr,$errnum);
 		}
-		
+
 		$this->content_type = curl_getinfo($curl,CURLINFO_CONTENT_TYPE);
 		$this->content_size = curl_getinfo($curl,CURLINFO_CONTENT_LENGTH_DOWNLOAD);
-		
+
 		curl_close($curl);
-		
+
 		// decompress if response is gzip encoded
 		if (strcmp(substr($response,0,2),"\x1f\x8b") == 0) {
 			$len1 = strlen($response);
@@ -195,23 +203,23 @@ class uber_api_client
 			} else {
 				$this->debug('Compression:','Raw:        '.$len2."\nCompressed: ".$len1."\nSaved:      ".'N/A');
 			}
-			
+
 			$this->content_size = $len2;
 		}
-		
+
 		$this->debug('Response:',$response);
-		
+
 		switch ($this->content_type) {
 			case 'application/json';
 				$result = json_decode($response,true);
 				if (!$result) {
 					return $this->raiseError('json error encountered: '. json_last_error(),-1);
 				}
-				
+
 				if (empty($result['status'])) {
 					return $this->raiseMethodError($result['error_message'],$result['error_code']);
 				}
-				
+
 				return $result['data'];
 			case 'application/xml':
 				return $response;
@@ -235,12 +243,12 @@ class uber_api_client
 				return $response;
 		}
 	}
-	
+
 	// flatten multi-dimensional params array
 	protected function curl_postfields($formdata,$numeric_prefix = '',$_parent = null)
 	{
 		$postdata = array();
-		
+
 		foreach ($formdata as $k => $v) {
 			if (!empty($_parent)) {
 				$k = $_parent .'['. $k .']';
@@ -253,37 +261,37 @@ class uber_api_client
 				$postdata[$k] = $v;
 			}
 		}
-		
+
 		return $postdata;
 	}
-	
+
 	// reads all the response headers one by one from curl
 	protected function read_header($ch, $header)
 	{
 		$len = strlen($header);
-		
+
 		$pos = stripos($header,'filename=');
 		if ($pos !== FALSE) {
 			$this->content_filename = substr($header,$pos + 9);
 		}
-		
+
 		return $len;
 	}
-	
+
 	// throw an UberException if an error occurs
 	protected function raiseError($text,$code = 1)
 	{
 		// we can throw exceptions from the SPL as well
 		throw new UberException($text,$code);
 	}
-	
+
 	// throw an UberMethodException if a method status was false
 	protected function raiseMethodError($text,$code = 1)
 	{
 		// we can throw exceptions from the SPL as well
 		throw new UberMethodException($text,$code);
 	}
-	
+
 	/**
 	 * internal function for displaying debug information
 	 */
@@ -339,7 +347,7 @@ if (!function_exists('gzdecode')) {
 			$extra = substr($data,10,$extralen);
 			$headerlen += 2 + $extralen;
 		}
-		
+
 		$filenamelen = 0;
 		$filename = "";
 		if ($flags & 8) {
@@ -354,7 +362,7 @@ if (!function_exists('gzdecode')) {
 			$filename = substr($data,$headerlen,$filenamelen);
 			$headerlen += $filenamelen + 1;
 		}
-		
+
 		$commentlen = 0;
 		$comment = "";
 		if ($flags & 16) {
@@ -369,7 +377,7 @@ if (!function_exists('gzdecode')) {
 			$comment = substr($data,$headerlen,$commentlen);
 			$headerlen += $commentlen + 1;
 		}
-		
+
 		$headercrc = "";
 		if ($flags & 1) {
 			// 2-bytes (lowest order) of CRC32 on header present
@@ -384,13 +392,13 @@ if (!function_exists('gzdecode')) {
 			}
 			$headerlen += 2;
 		}
-		
+
 		// GZIP FOOTER - These be negative due to PHP's limitations
 		$datacrc = unpack("V",substr($data,-8,4));
 		$datacrc = $datacrc[1];
 		$isize = unpack("V",substr($data,-4));
 		$isize = $isize[1];
-		
+
 		// Perform the decompression:
 		$bodylen = $len-$headerlen-8;
 		if ($bodylen < 1) {
@@ -413,22 +421,22 @@ if (!function_exists('gzdecode')) {
 			// I'm not sure if zero-byte body content is allowed.
 			// Allow it for now...  Do nothing...
 		}
-		
+
 		// Verify decompressed size and CRC32:
 		// NOTE: This may fail with large data sizes depending on how
 		//      PHP's integer limitations affect strlen() since $isize
 		//      may be negative for large sizes.
-		
+
 		$crc = crc32($data);
 		if ($crc > $datacrc) {
 			$crc -= 4294967296;
 		}
-		
+
 		if ($isize != strlen($data) || $crc != $datacrc) {
 			// Bad format!  Length or CRC doesn't match!
 			return false;
 		}
-		
+
 		return $data;
 	}
 }
